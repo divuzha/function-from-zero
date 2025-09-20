85802707e61c36eafb38ce209bf8e63678e04903
Đinh Vũ Gia Hân <128356619+divuzha@users.noreply.github.com>
2025-09-20 03:27:39 +0000

    adding file

---
+++ myStatements.py
@@ -0,0 +1,14 @@
+my_list = ["apple", "cherry", "lemon"]
+my_dict = {
+    "drink": "coffee",
+    "milk": "whole"
+}
+
+for item in my_list:
+    print(f"Eat {item}")
+
+for _, value in my_dict.items():
+    print(f"Drink {value}")
+
+# my_dict = my_dict
+# my_list2 =
